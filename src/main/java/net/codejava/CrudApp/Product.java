8d30afc2e9f9e32a1ff46c5e1ac9e025bea42bf1
Sayem Hasan <sayem.hasan@ntdac.naztech.local>
2019-06-23 18:40:25 +0600

    FIRST COMMIT

---
+++ src/main/java/net/codejava/CrudApp/Product.java
@@ -0,0 +1,67 @@
+package net.codejava.CrudApp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="sayem_product")
+public class Product {
+	private long id;
+	private String name;
+	private String brand;
+	private String madein;
+	private float price;
+	
+	protected Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getMadein() {
+		return madein;
+	}
+
+	public void setMadein(String madein) {
+		this.madein = madein;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+}
